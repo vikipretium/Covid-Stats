678890f026707a1c7e9f6fbd91ca90c1d2397bd4
vikipretium <vighneshpasupathy@gmail.com>
2022-04-21 00:25:23 +0530

    fix linters

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
-export default () => (
+const LoadingSpinner = () => (
   <div className="inline-block w-4 h-4 border-b-2 border-t-2 rounded-full animate-spin" />
 );
+export default LoadingSpinner;
