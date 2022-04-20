15b8663e4cbf0f770bbd6fb2e3d4a849f602994b
vikipretium <vighneshpasupathy@gmail.com>
2022-04-20 23:08:33 +0530

    add pages and components

---
+++ src/components/CountryTile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CountryTile() {
+  return (
+    <Link
+      to="/"
+      className="flex flex-col justify-end aspect-square border border-pink-600 p-2 text-right"
+    >
+      <h2 className="font-bold text-lg uppercase">Country Name</h2>
+      <p>50</p>
+    </Link>
+  );
+}
+
+export default CountryTile;
