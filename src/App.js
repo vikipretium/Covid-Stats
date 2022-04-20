97375209c343b91c8ec0c4cfa0db951d36c6d38f
vikipretium <vighneshpasupathy@gmail.com>
2022-04-21 00:22:38 +0530

    add basic routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import AllCountries from './pages/AllCountries';
+import CountryPage from './pages/CountryPage';
+
 function App() {
-  return <div className="App">base</div>;
+  return (
+    <div className="min-h-screen bg-pink-400 text-white">
+      <Header />
+      <Routes>
+        <Route path="/" element={<AllCountries />} />
+        <Route path="/countries/:countryName" element={<CountryPage />} />
+        <Route
+          path="*"
+          element={(
+            <main className="p-4">
+              <p>Not exist!</p>
+            </main>
+          )}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
