5c6c4dc9a2d35e91008735c2a895580b32c2fd52
vikipretium <vighneshpasupathy@gmail.com>
2022-04-21 17:51:11 +0530

    fetch and render data from api

--- src/pages/CountriesPage.js
+++ src/pages/CountriesPage.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import CountryTile from '../components/CountryTile';
+import CountriesList from '../components/CountriesList';
 
 function AllCountries() {
   return (
     <main className="container mx-auto">
       <div className="h-48">World Stats</div>
       <div className="p-1">Countries Stats</div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-        {[0, 1, 2, 3, 4].map((index) => (
-          <CountryTile key={index} />
-        ))}
-      </div>
+      <CountriesList />
     </main>
   );
 }
