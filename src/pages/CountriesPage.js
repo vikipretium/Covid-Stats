d8fe4425f46fc60af0fb0ef9a1bb05c458bee62c
vikipretium <vighneshpasupathy@gmail.com>
2022-04-22 23:03:15 +0530

    add background for main page

--- src/pages/CountriesPage.js
+++ src/pages/CountriesPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import CountriesList from '../components/CountriesList';
+import { getMapUrl } from '../modules/mapUtils';
 
 function AllCountries() {
+  const imageUrl = getMapUrl('world');
+
   return (
     <main className="container mx-auto flex flex-col grow">
-      <div className="h-48 grid place-content-center">World Stats</div>
+      <div className="h-48 md:h-64 grid place-content-center relative text-center">
+        <div
+          className="absolute inset-0 z-0 opacity-50"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+          }}
+        />
+        <h2 className="z-10 text-center text-3xl">World Covid-19 Cases</h2>
+      </div>
       <div className="bg-pink-500 grow rounded-t-3xl border-t border-t-pink-600">
         <div className="p-2 text-center uppercase font-bold">
           Cases of Countries
