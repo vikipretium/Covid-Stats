8f73c00c442f8e8c23214cc4020c4ddfa195bb4a
vikipretium <vighneshpasupathy@gmail.com>
2022-04-22 20:15:38 +0530

    add style to pages

--- src/pages/CountriesPage.js
+++ src/pages/CountriesPage.js
@@ -3,10 +3,14 @@ import CountriesList from '../components/CountriesList';
 
 function AllCountries() {
   return (
-    <main className="container mx-auto">
-      <div className="h-48">World Stats</div>
-      <div className="p-1">Countries Stats</div>
-      <CountriesList />
+    <main className="container mx-auto flex flex-col grow">
+      <div className="h-48 grid place-content-center">World Stats</div>
+      <div className="bg-pink-500 grow rounded-t-3xl border-t border-t-pink-600">
+        <div className="p-2 text-center uppercase font-bold">
+          Cases of Countries
+        </div>
+        <CountriesList />
+      </div>
     </main>
   );
 }
