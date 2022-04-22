a85d373c6316fbd763057dc8cb5077dc0a33eb72
vikipretium <vighneshpasupathy@gmail.com>
2022-04-22 18:46:32 +0530

    add regions using actions

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,20 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import {
+  applyMiddleware, combineReducers, compose, createStore,
+} from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import countriesReducer from './countries/countries';
+import regionsReducer from './regions/regions';
 
 const rootReducer = combineReducers({
   countries: countriesReducer,
+  regions: regionsReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk)),
+);
 
 export default store;
