1c416ffec2bccd16f630d1ba053a261b8dfb13b5
vikipretium <vighneshpasupathy@gmail.com>
2022-04-22 23:33:24 +0530

    add stats to coutries

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -3,7 +3,7 @@ import { PropTypes } from 'prop-types';
 
 const LoadingSpinner = ({ size = '' }) => {
   const sizeClasses = size.length ? size : 'w-6 h-6';
-  console.log(size);
+
   return (
     <div
       className={`inline-block ${sizeClasses} border-white border-2 border-t-white/0 border-r-white/25 border-b-white/60 rounded-full animate-spin`}
